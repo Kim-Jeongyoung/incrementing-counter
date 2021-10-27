7ac01d3a3ebcf9e57e055387583a01e1fb113dd3
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-10-26 21:04:44 -0700

    Added target

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ counters.forEach((counter) => {
   counter.innerText = '0';
 
   const updateCounter = () => {
-    const target = counter.getAttribute('data-target');
-    console.log(typeof target, target);
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+    const increment = target / 1500;
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
   };
   updateCounter();
 });
